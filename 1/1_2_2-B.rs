8d764f377edee8638f1871e310c2a8d458f5dd43
Joseph Marlin <j.marlin@outlook.com>
2016-04-21 16:15:54 -0400

    Did end activity for showing complex numbers in 1_2_2-B.rs

--- 1/1_2_2-B.rs
+++ 1/1_2_2-B.rs
@@ -28,6 +28,22 @@ impl fmt::Display for Point2 {
     }
 }
 
+// Define a structure to represent complex numbers
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64
+}
+
+// Implement standard print interpolation for Complex
+impl fmt::Display for Complex {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        
+        write!(f, "{} + {}i", self.real, self.imag);
+    }
+}
+
 fn main() {
     let minmax = MinMax(0, 14);
     
@@ -51,4 +67,10 @@ fn main() {
     // Error. Both 'Debug' and 'Display' were implemented but '{:b}'
     // requires 'fmt::Binary' to be implemented. This will not work.
     // println!("What does Point2D look like in binary: {:b}?", point);
+    
+    let complex = Complex { real: 3.3, imag: 7.2 };
+    
+    println!("Compare complexes:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
 }
